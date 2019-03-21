4bc103937523d0ecd6f7aff9055848027aa83e70
CursoAngularUdemy <48363996+CursoAngularUdemy@users.noreply.github.com>
2019-03-20 20:49:30 -0600

    Create variables.ts

---
+++ TypeScript/variables.ts
@@ -0,0 +1,22 @@
+var num1 = 10;
+let num2 = 20;
+
+console.log(num1);
+console.log(num2);
+
+
+function variables(){
+    var num3 = 30;
+
+    if (num3 > 10)
+    {
+        var  num4 = 4;  // Esta variable solo sera visible dentro de IF
+        console.log (num4) 
+    }
+    console.log(num3);
+    console.log(num4);
+}
+
+
+variables();
+
